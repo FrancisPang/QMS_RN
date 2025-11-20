59b96bd1f9386692c210cd137486117937082f97
Alep <alepmustaqim03@gmail.com>
2025-11-20 22:54:03 +0800

    fix: added new module

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -32,6 +32,7 @@
 // });
 
 import React from 'react';
+
 import {
   StatusBar,
   useColorScheme,
@@ -39,15 +40,25 @@ import {
   View,
   StyleSheet,
 } from 'react-native';
-import { QmsDashboardView } from 'react-native-qms-plugin';
+
+import { QmsDashboardView } from '@convep_mobilogy/react-native-qms-plugin';
 
 export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
+  const clientID = '<client_id>';
+  const userToken = '<token>';
+  const clientCode = '<client_code>';
   return (
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+
       <View style={styles.container}>
-        <QmsDashboardView style={styles.container} />
+        <QmsDashboardView
+          style={styles.container}
+          clientID={clientID}
+          clientCode={clientCode}
+          userToken={userToken}
+        />
       </View>
     </SafeAreaView>
   );
