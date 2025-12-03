e9c925d711d1cd34f43054c2c1c591107c16cbf9
Alep <alepmustaqim03@gmail.com>
2025-12-03 11:57:09 +0800

    fix: update code

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -11,7 +11,8 @@ import { QmsDashboardView } from '@convep_mobilogy/react-native-qms-plugin';
 export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
   const clientID = '3';
-  const userToken = '<token>';
+  const userToken =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImF0aGlyYWh6YWlkaUBjb252ZXAuY29tIiwicGFzc3dvcmQiOiIkMnkkMTAkNU9xYklqMnZ6UHJwckJrcVd5c3I2LmJCc1hVYS9Hd014eUhnL3RhUUhPUkNQcGdmTG8yakciLCJzdWIiOjIxNzAsImlzcyI6Imh0dHBzOi8va2l0YWRldi5jb21tdWRlc2suY29tL2FwaS9vd25lci9hdXRoL3Bhc3N3b3JkbGVzc19sb2dpbiIsImlhdCI6MTc2MzUzNTI3MywiZXhwIjoyMDc5MTA0NzkzLCJuYmYiOjE3NjM1MzUyNzMsImp0aSI6IkFpS3Jha3BydDI4TzBEQ3MifQ.A5RHPiy6dOSQQOtnTLd_NKsKtcu1KWhQqkK1MI2a9wM';
   const clientCode = 'KITADEV';
   return (
     <SafeAreaView>
